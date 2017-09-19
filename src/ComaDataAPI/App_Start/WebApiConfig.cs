6e5bb570dfd33d72bfb2a75e66dd13329c708f75
Dmitri Lapchine <dimokhot@hotmail.com>
2017-09-18 22:20:15 -0400

    Stage 1: Pilot

--- src/ComaDataAPI/App_Start/WebApiConfig.cs
+++ src/ComaDataAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.OData.Extensions;
 using Microsoft.Restier.Providers.EntityFramework;
 using Microsoft.Restier.Publishers.OData;
