99c7cce21063fca2bb62ce384d4846453cc65780
Dmitri Lapchine <dimokhot@hotmail.com>
2017-09-20 09:02:42 -0400

    FileController

---
+++ src/ComaDataAPI/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Foundation;
+using Foundation.CustomConfig;
+
+namespace ComaDataAPI.Controllers {
+  [RoutePrefix("api/files")]
+  public class FileController : ApiController {
+    class Configer : Config<Configer> {
+      [appSettings]
+      public string DataPath => KeyValue();
+    }
+    [Route("HelloWorld")]
+    public IHttpActionResult Get() {
+      return Ok(from path in CommonExtensions.Helpers.FindFiles("*.txt","App_Data")
+              select new { path, text = System.IO.File.ReadAllText(path) });
+    }
+  }
+}
