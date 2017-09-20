837af3791c27b8e23bd4b1ed20c53c048561bf30
dimokgit <dimokhot@hotmail.com>
2017-09-20 09:06:21 -0400

    Update FileController.cs

--- src/ComaDataAPI/Controllers/FileController.cs
+++ src/ComaDataAPI/Controllers/FileController.cs
@@ -13,6 +13,43 @@ namespace ComaDataAPI.Controllers {
     class Configer : Config<Configer> {
       [appSettings]
       public string DataPath => KeyValue();
+    #region RunTest
+    string testRoot { get { return GetType().FullName; } }
+    //          var url = JiraMonad.JiraServiceBaseAddress();
+    protected override async Task<ExpandoObject> _RunTestFastAsync(ExpandoObject parameters, params Func<ExpandoObject, ExpandoObject>[] merge) {
+      return await TestHostAsync(parameters, (p, m) =>
+        (from rf in base._RunTestFastAsync(parameters, merge)
+         from ut in Task.Run(() => { JiraMonad.UnTest(); return new ExpandoObject(); })
+         from jm in JiraMonad.RunTestFastAsync(parameters, merge)
+         select new ExpandoObject().Merge(testRoot, jm.Merge(rf).Merge(new { DataPath }))
+        )
+      , _LogError, merge);
+    }
+
+    protected override async Task<ExpandoObject> _RunTestAsync(ExpandoObject parameters, params Func<ExpandoObject, ExpandoObject>[] merge) {
+      return await TestHostAsync(parameters, async (p, m) => {
+        var e = await base._RunTestAsync(parameters, merge);
+        var self = await RestMonad.Empty().GetMySelfAsync();
+        var l = new { self }.ToExpando();
+        if (string.IsNullOrWhiteSpace(DataPath))
+          l = l.Merge(new { DataPath });
+        else {
+          try {
+            var appDataPath = Path.Combine(DataPath, Path.GetRandomFileName());
+            var testFile = JiraController.ServerPath(appDataPath) ?? Helpers.MapExecPath(appDataPath);
+            using (var s = File.CreateText(testFile))
+              s.WriteLine("xxx");
+            File.Delete(testFile);
+            l = l.Merge(new { dataPath = new { testFile, access = "Read/Write" } });
+          }
+          catch (Exception exc) {
+            throw new Exception(new { ConfigValue = new { DataPath } } + "", exc);
+          }
+        }
+        return e.AddOrMerge(testRoot, l);
+      }, _LogError, merge);
+    }
+    #endregion
     }
     [Route("HelloWorld")]
     public IHttpActionResult Get() {
